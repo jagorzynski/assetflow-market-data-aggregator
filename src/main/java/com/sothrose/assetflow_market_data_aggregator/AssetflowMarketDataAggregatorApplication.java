ec8315ed5aac71bd1d5240c77ebdee89920b8875
Gorzynski Jakub <gorzynski.jakub@protonmail.com>
2024-12-27 18:36:30 +0100

    init commit

---
+++ src/main/java/com/sothrose/assetflow_market_data_aggregator/AssetflowMarketDataAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.sothrose.assetflow_market_data_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetflowMarketDataAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetflowMarketDataAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sothrose/assetflow_market_data_aggregator/AssetflowMarketDataAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sothrose.assetflow_market_data_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetflowMarketDataAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
